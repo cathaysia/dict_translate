cf24ffc8d4e6acb8af66b6c4955b1d6be6982994
Longtao Zhang <DragonBillow@outlook.com>
2023-07-09 08:57:39 +0800

    feat(rime): 更改了函数名、添加了单元测试、修复了 rime 解析问题
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ struct Line<'a> {
     weight: &'a str,
 }
 
-fn convert_tuple<'a>(data: &'a str) -> Vec<Line> {
+/// Rime 词库包含以下几种格式：
+/// - word
+/// - word \t\t weight
+/// - word \t pinyin
+/// - word \t pinyin \t weight
+///
+/// * `data`: rime dict file content
+fn rime2lines<'a>(data: &'a str) -> Vec<Line> {
     data.lines()
         .into_iter()
         .filter(|line| !line.starts_with("#"))
@@ -33,7 +40,7 @@ fn convert_tuple<'a>(data: &'a str) -> Vec<Line> {
                 _ => Line::default(),
             }
         })
-        .filter(|item| item.word != "")
+        .filter(|item| item.word != "" && item.pinyin != "")
         .collect()
 }
 
@@ -47,8 +54,37 @@ fn main() {
 
     args.files.iter().for_each(|filename| {
         let data = fs::read_to_string(filename).unwrap();
-        convert_tuple(&data)
+        rime2lines(&data)
             .iter()
             .for_each(|item| println!("{}\t{}\tzh-CN", item.word, item.pinyin));
     });
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rime_convert_to_lines() {
+        let line1 = "〇	ling\nNFO	n f o";
+        let line2 = "阿母大悲摧";
+        let line3 = "阿姑阿翁	a gu a weng	1";
+        let line4 = "一定可以		1";
+
+        let data1 = rime2lines(&line1);
+        assert_eq!(2, data1.len());
+        assert_eq!("ling", data1[0].pinyin);
+        assert_eq!("n f o", data1[1].pinyin);
+
+        let data2 = rime2lines(&line2);
+        assert_eq!(0, data2.len());
+
+        let data3 = rime2lines(&line3);
+        assert_eq!(1, data3.len());
+        assert_eq!("a gu a weng", data3[0].pinyin);
+        assert_eq!(1, data3[0].weight.parse::<i32>().unwrap());
+
+        let data4 = rime2lines(&line4);
+        assert_eq!(0, data4.len());
+    }
 }
